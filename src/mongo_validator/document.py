306a1bbd0d180a6154729461c2930195c14f5470
colton <lwcolton@gmail.com>
2016-01-02 13:43:17 -0500

    add test setup and plyint works

--- src/mongo_validator/document.py
+++ src/mongo_validator/document.py
@@ -5,23 +5,7 @@ import cerberus
 class Document(collections.UserDict):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
-		self._schema_dict = {}
-		schema_class = self.schema
-		for schema_attr_name in dir(schema_class):
-			if schema_attr_name.startswith("_"):
-				continue
-			if schema_attr_name in ignore_attrs:
-				continue
-			schema_attr = getattr(schema_attr_name, schema_class)
-			if isinstance(schema_attr, collections.Mapping):
-				self._schema_dict.update(schema_attr)
-			if hasattr(schema_attr, '__call__'):
-				self._schema_dict.update(schema_attr())
-			else:
-				raise ValueError("Unknown type for field {0}: {1}".format(
-						schema_attr_name, type(schema_attr)
-					)
-				)
+		self._schema_dict = self._get_schema_dict()
 
 	def validate(self):
 		validator = cerberus.Validator(self._schema_dict)
@@ -30,9 +14,34 @@ class Document(collections.UserDict):
 		else:
 			return validator.errors
 
+	def _get_schema_dict(self):
+		if hasattr(self, "schema"):
+			schema = getattr(self, "schema")
+			if isinstance(schema, collections.Mapping):
+				return schema
+			elif issubclass(schema, Schema):
+				return self._get_schema_dict_from_class(schema)
+			else:
+				return ValueError(
+					"Unknown type for schema attribute: {0}".format(type(schema))
+				)
 
-
-		
-
-
-
+	@classmethod
+	def _get_schema_dict_from_class(cls, schema_class):
+		"""Retrieves a cerberus schema dict from a Schema class"""
+		schema_class = self.schema
+		schema_dict = {}
+		for schema_attr_name in dir(schema_class):
+			if schema_attr_name.startswith("_"):
+				continue
+			schema_attr = getattr(schema_class, schema_attr_name)
+			if isinstance(schema_attr, collections.Mapping):
+				schema_dict[schema_attr_name] = schema_attr
+			elif hasattr(schema_attr, '__call__'):
+				schema_dict[schema_attr_name] = schema_attr()
+			else:
+				raise ValueError("Unknown type for field {0}: {1}".format(
+						schema_attr_name, type(schema_attr)
+					)
+				)
+		return schema_dict
